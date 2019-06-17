5f24b601a98e66f8d8dc5976cd93ccd780c0652d
59398 <59398@LAPTOP-32FRB211>
2019-06-17 07:15:25 -0500

    clase controlador para la lectura y escritura

--- src/ec/edu/ups/controladores/ControladorTextoIngresado.java
+++ src/ec/edu/ups/controladores/ControladorTextoIngresado.java
@@ -19,44 +19,39 @@ import java.util.List;
 
 /**
  *
- * @author 59398
+ * @author QuezadaBryam
  */
 public class ControladorTextoIngresado {
+
     String ruta = "archivito.txt";
-       private String linea;
+    private String linea;
     private List<Texto> lista;
 
     public ControladorTextoIngresado() {
         lista = new ArrayList<>();
     }
-    
-    /**
-     * Metodo que sirve para Escribir en un archivo
-     * @param palabra
-     * @throws IOException 
-     */
-    public void Escritura(String palabra) throws IOException{
-          try {
-         FileWriter archivo = new FileWriter(ruta, true);
 
-         BufferedWriter escritura = new BufferedWriter(archivo);
-         escritura.append(palabra);
-         escritura.newLine();
-         
-         escritura.append(palabra);
-         
-         escritura.close();
-         archivo.close();
-          } catch (IOException ex) {
-         System.err.println("\n" + "Error de escritura");
-         }
-      
+    //metodo implementado para la escritura del archivo
+    public void Escritura(String palabra) throws IOException {
+        try {
+            FileWriter archivo = new FileWriter(ruta, true);
+
+            BufferedWriter escritura = new BufferedWriter(archivo);
+            escritura.append(palabra);
+            escritura.newLine();
+
+            escritura.append(palabra);
+
+            escritura.close();
+            archivo.close();
+        } catch (IOException ex) {
+            System.out.println("\n" + "Error al escribir");
+        }
+
     }
-    /**
-     * Metodo que nos devuelve todo lo que posee un archivo
-     */
-    public void Lectura(){
-         try {
+    //metodo implementado para la lectura del archivos
+    public void Lectura() {
+        try {
 
             FileReader archivo = new FileReader(ruta);
             BufferedReader lectura = new BufferedReader(archivo);
@@ -64,91 +59,86 @@ public class ControladorTextoIngresado {
 
             while (linea != null) {
                 linea = lectura.readLine();
-                if(linea != null){
+                if (linea != null) {
                     System.out.println(linea);
                 }
-               
+
             }
 
         } catch (FileNotFoundException ex1) {
             System.out.println("Archivo " + ruta + "no encontrada");
         } catch (IOException ex) {
-            System.out.println("error de lectura");
+            System.out.println("error al leer");
         }
     }
-    /**
-     * Este metodo sirve para Leer la linea y luego lo compara
-     */
-    private void leerLinea() { 
+
+    //metodo implementado para la lectura de linea
+    private void leerLinea() {
         String palabras[] = linea.split(" ");
-         for(int i = 0; i <palabras.length;i++){
+        for (int i = 0; i < palabras.length; i++) {
             compararPalabra(palabras[i].toLowerCase());
         }
-     
+
     }
-    /**
-     * Metodo para comparar Palabras repetidas
-     * @param palabra 
-     */
+    
+    //metodo que compara las palabras repetidas
     private void compararPalabra(String palabra) {
         int con = 0;
-        for (Texto  palabralista: lista) {
+        for (Texto palabralista : lista) {
             if (palabralista.getNombre().equals(palabra)) {
-                palabralista.setCantidad(palabralista.getCantidad()+1);
+                palabralista.setCantidad(palabralista.getCantidad() + 1);
                 con = 1;
                 break;
             }
         }
-        if(con == 0){
+        if (con == 0) {
             Texto newPalabra = new Texto();
             newPalabra.setNombre(palabra);
             newPalabra.setCantidad(1);
             lista.add(newPalabra);
         }
     }
-    /**
-     * Este metodo guarda en una archivo resultado las veces repetidas 
-     * 
-     * @throws IOException 
-     */
+
+    //metodo que guarda el resultado
     public void guardaResultado() throws IOException {
-        Collections.sort(lista, new Comparator<Texto>(){
-            public int compare(Texto p1, Texto p2){
+        Collections.sort(lista, new Comparator<Texto>() {
+            public int compare(Texto p1, Texto p2) {
                 return p1.getNombre().compareTo(p2.getNombre());
             }
         });
         try {
             String ruta2 = "Resultado.txt";
             FileWriter archivo = new FileWriter(ruta2, false);
-            
+
             BufferedWriter escritura = new BufferedWriter(archivo);
             for (Texto palabraEscribir : lista) {
-                escritura.append(palabraEscribir.getNombre()+"   veces repetidas:"+palabraEscribir.getCantidad());
+                escritura.append(palabraEscribir.getNombre() + "   veces repetidas:" + palabraEscribir.getCantidad());
                 escritura.newLine();
             }
-            
+
             escritura.close();
             archivo.close();
-            
+
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Error de Escritura");
         }
     }
+
     public void LecturaResultado(String ruta1) throws IOException {
         linea = "";
-        try {           
-           FileReader archivo = new FileReader(ruta1);
+        try {
+            FileReader archivo = new FileReader(ruta1);
             BufferedReader lectura = new BufferedReader(archivo);
             while (linea != null) {
                 linea = lectura.readLine();
-                if(linea != null){
+                if (linea != null) {
                     leerLinea();
-                }              
-            }       
+                }
+            }
         } catch (FileNotFoundException ex) {
             System.out.println("El archivo no existe");
         } catch (IOException e) {
-            System.err.println("Error al momento de escribir");
+            System.out.println("Error al momento de escribir");
         }
 
     }
